7362dd13806a92c1335eef0ec68fce4601c8d402
Janna Hastings <janna.hastings@babraham.ac.uk>
2021-12-22 11:34:21 +0100

    First version of ontology

---
+++ src/templates/buildFromTemplate.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+
+curl -L http://purl.obolibrary.org/obo/MFOMD.owl --output MFOMD.owl
+curl -L http://purl.obolibrary.org/obo/MF.owl --output MF.owl
+
+robot merge --input MF.owl --input MFOMD.owl --output imports.owl
+
+
+robot template --input imports.owl  --prefix "OPHEP: http://purl.obolibrary.org/obo/OPHEP_" --ontology-iri "http://purl.obolibrary.org/obo/OPHEP.owl" --template ophep.csv --output ophep-template.owl
+
+rm MFOMD.owl
+rm MF.owl
+rm imports.owl 
+
+mv ophep-template.owl ../ontology/
